d88fbd23aeb66a301bc209d1c81140264d8775f2
Ta-Ching <life1347@gmail.com>
2016-02-04 20:32:35 +0800

    update import influxdb dep

--- notifier/influxdb-notifier.go
+++ notifier/influxdb-notifier.go
@@ -2,7 +2,7 @@ package notifier
 
 import (
 	log "github.com/AcalephStorage/consul-alerts/Godeps/_workspace/src/github.com/Sirupsen/logrus"
-	"github.com/AcalephStorage/consul-alerts/Godeps/_workspace/src/github.com/influxdb/influxdb/client"
+	"github.com/AcalephStorage/consul-alerts/Godeps/_workspace/src/github.com/influxdb/influxdb/client/v2"
 )
 
 type InfluxdbNotifier struct {
