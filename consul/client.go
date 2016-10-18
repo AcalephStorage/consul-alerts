4af687625cba5866291960dbe8d95ea7b703788f
Marco Supino <marco@supino.org>
2016-10-18 13:41:35 +0300

    update status of reminder if output changes

--- consul/client.go
+++ consul/client.go
@@ -268,7 +268,8 @@ func (c *ConsulAlertClient) UpdateCheckData() {
 		settodelete := true
 
 		for j := range nodecat {
-			if (nodecat[j].CheckID == check) && (nodecat[j].Status != "passing") {
+			# if (nodecat[j].CheckID == check) && (nodecat[j].Status != "passing") {
+			if nodecat[j].CheckID == check {
 				settodelete = false
 				break
 			}
@@ -324,6 +325,8 @@ func (c *ConsulAlertClient) UpdateCheckData() {
 				log.Printf("Updating reminder data for %s", reminderkey)
 
 				remindermap["Output"] = health.Output
+				remindermap["Status"] = health.Status
+
 				newreminder, _ := json.Marshal(remindermap)
 
 				_, err := kvApi.Put(&consulapi.KVPair{Key: reminderkey, Value: newreminder}, nil)
