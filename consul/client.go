dba7df8895f7e5f16e32daede42deae506b8b1f1
Roman Vynar <roman.vynar@centricient.com>
2017-02-03 02:54:09 +0200

    Dramatically reduce unnecessary writes (PUT requests) to KV when no change in health check status. (#166)

--- consul/client.go
+++ consul/client.go
@@ -275,10 +275,9 @@ func (c *ConsulAlertClient) UpdateCheckData() {
 		}
 		if settodelete {
 			log.Printf("Reminder %s %s needs to be deleted, stale", node, check)
-		        c.DeleteReminder(node, check)
+			c.DeleteReminder(node, check)
 		}
 	}
-
 
 	for _, health := range healths {
 
@@ -533,12 +532,16 @@ func (c *ConsulAlertClient) updateHealthCheck(key string, health *Check) {
 	// status is still pending for change. will change if it reaches threshold
 	stillPendingStatus := storedStatus.Current != health.Status && storedStatus.Pending == health.Status
 
+	// indicate whether we are changing storedStatus to prevent unnecessary PUT to KV
+	changed := false
+
 	switch {
 
 	case noStatusChange:
 		if storedStatus.Pending != "" {
 			storedStatus.Pending = ""
 			storedStatus.PendingTimestamp = time.Time{}
+			changed = true
 			log.Printf(
 				"%s:%s:%s is now back to %s.",
 				health.Node,
@@ -551,6 +554,7 @@ func (c *ConsulAlertClient) updateHealthCheck(key string, health *Check) {
 	case newPendingStatus:
 		storedStatus.Pending = health.Status
 		storedStatus.PendingTimestamp = time.Now()
+		changed = true
 		log.Printf(
 			"%s:%s:%s is now pending status change from %s to %s.",
 			health.Node,
@@ -578,6 +582,7 @@ func (c *ConsulAlertClient) updateHealthCheck(key string, health *Check) {
 			storedStatus.Pending = ""
 			storedStatus.PendingTimestamp = time.Time{}
 			storedStatus.ForNotification = true
+			changed = true
 		} else {
 			log.Printf(
 				"%s:%s:%s is pending status change from %s to %s for %s.",
@@ -592,6 +597,10 @@ func (c *ConsulAlertClient) updateHealthCheck(key string, health *Check) {
 
 	}
 	storedStatus.HealthCheck = health
+
+	if !changed {
+		return
+	}
 
 	data, _ := json.Marshal(storedStatus)
 	c.api.KV().Put(&consulapi.KVPair{Key: key, Value: data}, nil)
