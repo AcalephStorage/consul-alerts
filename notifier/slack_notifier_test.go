b7e68d8223bef40203a359de2785d820466d839d
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2017-09-10 14:29:08 +0200

    Add tests for SlackNotifier unmarshalling from json

---
+++ notifier/slack_notifier_test.go
@@ -0,0 +1,45 @@
+package notifier
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestJsonUnmarshalling(t *testing.T) {
+	cluster_name := "my_cluster"
+	channel := "test_channel"
+	username := "test_username"
+	icon_url := "test_icon_url"
+	icon_emoji := "test_icon_emoji"
+	text := "test_text"
+
+	expectedNotifier := SlackNotifier{
+		ClusterName: cluster_name,
+		Channel:     channel,
+		Username:    username,
+		IconUrl:     icon_url,
+		IconEmoji:   icon_emoji,
+		Text:        text,
+	}
+	var unmarshalledNotifier SlackNotifier
+
+	data := []byte(fmt.Sprintf(`{
+    "cluster_name": "%s",
+    "channel": "%s",
+    "username": "%s",
+    "icon_url": "%s",
+    "icon_emoji": "%s",
+    "text": "%s"
+  }`, cluster_name, channel, username, icon_url, icon_emoji, text))
+
+	fmt.Printf("%s\n", data)
+	if err := json.Unmarshal(data, &unmarshalledNotifier); err != nil {
+		t.Error(err.Error())
+	}
+
+	if !reflect.DeepEqual(expectedNotifier, unmarshalledNotifier) {
+		t.Fatalf("Expected slackNotifier to be %s, got %s\n", expectedNotifier, unmarshalledNotifier)
+	}
+}
